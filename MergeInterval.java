c7b9b071124b5ba8c2fb4fa97101596de576dc2f
Saberfor <435257945@qq.com>
2022-01-12 18:38:57 +0800

    合并区间

---
+++ MergeInterval.java
@@ -0,0 +1,49 @@
+package com.gxc.likou;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 合并区间
+ */
+public class MergeInterval {
+
+    public static void main(String[] args) {
+        int[][] intervals = new int[4][2];
+        intervals[1] = new int[]{0,2};
+        intervals[3] = new int[]{1,4};
+        intervals[2] = new int[]{3,5};
+        intervals[0] = new int[]{15,18};
+       /* Arrays.stream(intervals).forEach(a ->{
+            System.out.println(a[0] + "," + a[1]);
+        });*/
+        Arrays.stream(merge(intervals)).forEach(a ->{
+            System.out.println(a[0] + "," + a[1]);
+        });
+
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> re = new ArrayList<>();
+        int max=0;
+        int min=0;
+        int[] compare = intervals[0];
+        for (int i=1;i<intervals.length;i++){
+            if (compare[1]>=intervals[i][0]) {
+                min = Integer.min(compare[0], intervals[i][0]);
+                max = Integer.max(compare[1], intervals[i][1]);
+                compare = new int[]{min, max};
+            } else {
+                re.add(compare);
+                compare=intervals[i];
+            }
+
+        }
+        re.add(compare);
+        return re.toArray(new int[re.size()][2]);
+    }
+
+}
